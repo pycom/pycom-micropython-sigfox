6f11a5cb0550ebe8bceb763e16a58893024a7a98
Sylvain Miermont <smiermont@semtech.com>
2013-10-11 14:38:15 +0200

    Replaced static configuration by a parser for JSON configuration files; Added an example of global and local configuration files.

--- loragw_pkt_logger/src/loragw_pkt_logger.c
+++ loragw_pkt_logger/src/loragw_pkt_logger.c
@@ -28,9 +28,10 @@ Description:
 #include <string.h>		/* memset */
 #include <signal.h>		/* sigaction */
 #include <time.h>		/* time clock_gettime strftime gmtime clock_nanosleep*/
-#include <unistd.h>		/* getopt */
+#include <unistd.h>		/* getopt access */
 #include <stdlib.h>		/* atoi */
 
+#include "parson.h"
 #include "loragw_hal.h"
 
 /* -------------------------------------------------------------------------- */
@@ -46,7 +47,7 @@ Description:
 #define	LORANET_MAC_OFFSET		4
 
 /* -------------------------------------------------------------------------- */
-/* --- PRIVATE VARIABLES ---------------------------------------------------- */
+/* --- PRIVATE VARIABLES (GLOBAL) ------------------------------------------- */
 
 char str_buf[256]; /* temporary buffer to build and manipulate strings */
 struct timespec sleep_time = {0, 3000000}; /* 3 ms */
@@ -56,12 +57,8 @@ struct sigaction sigact; /* SIGQUIT&SIGINT&SIGTERM signal handling */
 static int exit_sig = 0; /* 1 -> application terminates cleanly (shut down hardware, close open files, etc) */
 static int quit_sig = 0; /* 1 -> application terminates without shutting down the hardware */
 
-/* configuration file related */
-char conf_file_name[64] = "loragw_conf.json"; /* default configuration file */
-FILE * conf_file;
-
-/* default configuration */
-uint64_t lgwm = 0xAA555A0000000000; /* Lora gateway MAC address */
+/* configuration variables needed by the application  */
+uint64_t lgwm = 0; /* Lora gateway MAC address */
 char lgwm_str[17];
 
 /* clock, log file and log rotation management */
@@ -73,19 +70,14 @@ int time_check = 0; /* variable used to limit the number of calls to time() func
 char log_file_name[64];
 unsigned long pkt_in_log; /* count the number of packet written in each log file */
 
-/* allocate memory for packet fetching and processing */
-struct lgw_pkt_rx_s rxpkt[16]; /* array containing up to 16 inbound packets metadata */
-struct lgw_pkt_rx_s *p; /* pointer on a RX packet */
-int nb_pkt;
-
-/* variables for packet parsing and logging */
-
 /* -------------------------------------------------------------------------- */
 /* --- PRIVATE FUNCTIONS DECLARATION ---------------------------------------- */
 
 static void sig_handler(int sigio);
 
-void set_loragw_default_conf(void);
+int parse_SX1301_configuration(const char * conf_file);
+
+int parse_gateway_configuration(const char * conf_file);
 
 void open_log(void);
 
@@ -100,100 +92,198 @@ static void sig_handler(int sigio) {
 	}
 }
 
-void set_loragw_default_conf(void) {
+int parse_SX1301_configuration(const char * conf_file) {
 	int i;
-	int err = 0;
+	const char conf_obj[] = "SX1301_conf";
+	char param_name[32]; /* used to generate variable parameter names */
 	struct lgw_conf_rxrf_s rfconf;
 	struct lgw_conf_rxif_s ifconf;
+	JSON_Value *root_val;
+	JSON_Object *root = NULL;
+	JSON_Object *conf = NULL;
+	JSON_Value *val;
+	char * tmp_string;
+	uint32_t sf, bw;
+	double tmp_int;
+	int tmp_bool;
 	
-	memset(&rfconf, 0, sizeof(rfconf));
-	memset(&ifconf, 0, sizeof(ifconf));
-	
-	/* set configuration for RF chains */
-	rfconf.enable = true;
-	rfconf.freq_hz = 866250000;
-	i = lgw_rxrf_setconf(0, rfconf); /* radio A */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for radio A\n");
-		++err;
+	/* try to parse JSON */
+	root_val = json_parse_file(conf_file);
+	root = json_value_get_object(root_val);
+	if (root == NULL) {
+		MSG("ERROR: %s id not a valid JSON file\n", conf_file);
+		exit(EXIT_FAILURE);
+	}
+	conf = json_object_get_object(root, conf_obj);
+	if (conf == NULL) {
+		MSG("INFO: %s does not contain a JSON object named %s\n", conf_file, conf_obj);
+		return -1;
+	} else {
+		MSG("INFO: %s does contain a JSON object named %s, parsing SX1301 parameters\n", conf_file, conf_obj);
 	}
 	
-	rfconf.enable = true;
-	rfconf.freq_hz = 866500000;
-	i = lgw_rxrf_setconf(1, rfconf); /* radio B */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for radio B\n");
-		++err;
+	/* set configuration for RF chains */
+	for (i = 0; i < LGW_RF_CHAIN_NB; ++i) {
+		memset(&rfconf, 0, sizeof(rfconf)); /* initialize configuration structure */
+		sprintf(param_name, "radio_%i", i); /* compose parameter path inside JSON structure */
+		val = json_object_get_value(conf, param_name); /* fetch value (if possible) */
+		if (json_value_get_type(val) != JSONObject) {
+			MSG("INFO: no configuration for radio %i\n", i);
+			continue;
+		}
+		/* there is an object to configure that radio, let's parse it */
+		sprintf(param_name, "radio_%i.enable", i);
+		val = json_object_dotget_value(conf, param_name);
+		if (json_value_get_type(val) != JSONBoolean) {
+			rfconf.enable = (bool)json_value_get_boolean(val);
+		}
+		if (rfconf.enable == false) { /* radio disabled, nothing else to parse */
+			MSG("INFO: radio %i disabled\n", i);
+		} else  { /* radio enabled, will parse the other parameters */
+			sprintf(param_name, "radio_%i.freq", i);
+			rfconf.freq_hz = (uint32_t)json_object_dotget_number(conf, param_name);
+			MSG("INFO: radio %i enabled, center frequency %u\n", i, rfconf.freq_hz);
+		}
+		/* all parameters parsed, submitting configuration to the HAL */
+		if (lgw_rxrf_setconf(i, rfconf) != LGW_HAL_SUCCESS) {
+			MSG("WARNING: invalid configuration for radio %i\n", i);
+		}
 	}
 	
 	/* set configuration for Lora multi-SF channels (bandwidth cannot be set) */
-	
-	ifconf.enable = true;
-	ifconf.rf_chain = 0;
-	ifconf.freq_hz = -187500;
-	ifconf.datarate = DR_LORA_MULTI;
-	i = lgw_rxif_setconf(0, ifconf); /* chain 0: Lora 125kHz, all SF, on 865.7 MHz */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for Lora multi-SF channel 0\n");
-		++err;
+	for (i = 0; i < LGW_MULTI_NB; ++i) {
+		memset(&ifconf, 0, sizeof(ifconf)); /* initialize configuration structure */
+		sprintf(param_name, "chan_multiSF_%i", i); /* compose parameter path inside JSON structure */
+		if (json_value_get_type(json_object_get_value(conf, param_name)) != JSONObject) {/* fetch value (if possible) */
+			MSG("INFO: no configuration for Lora multi-SF channel %i\n", i);
+			continue;
+		}
+		/* there is an object to configure that Lora multi-SF channel, let's parse it */
+		sprintf(param_name, "chan_multiSF_%i.enable", i);
+		val = json_object_dotget_value(conf, param_name);
+		if (json_value_get_type(val) == JSONBoolean) {
+			ifconf.enable = (bool)json_value_get_boolean(val);
+		}
+		if (ifconf.enable == false) { /* Lora multi-SF channel disabled, nothing else to parse */
+			MSG("INFO: Lora multi-SF channel %i disabled\n", i);
+		} else  { /* Lora multi-SF channel enabled, will parse the other parameters */
+			sprintf(param_name, "chan_multiSF_%i.radio", i);
+			ifconf.rf_chain = (uint32_t)json_object_dotget_number(conf, param_name);
+			sprintf(param_name, "chan_multiSF_%i.if", i);
+			ifconf.freq_hz = (int32_t)json_object_dotget_number(conf, param_name);
+			// TODO: handle individual SF enabling and disabling (spread_factor)
+			MSG("INFO: Lora multi-SF channel %i enabled, radio %i selected, IF %i Hz, 125 kHz bandwidth, SF 7 to 12\n", i, ifconf.rf_chain, ifconf.freq_hz);
+		}
+		/* all parameters parsed, submitting configuration to the HAL */
+		if (lgw_rxif_setconf(i, ifconf) != LGW_HAL_SUCCESS) {
+			MSG("WARNING: invalid configuration for Lora multi-SF channel %i\n", i);
+		}
 	}
 	
-	ifconf.enable = true;
-	ifconf.rf_chain = 0;
-	ifconf.freq_hz = -62500;
-	ifconf.datarate = DR_LORA_MULTI;
-	i = lgw_rxif_setconf(1, ifconf); /* chain 1: Lora 125kHz, all SF, on 866.3 MHz */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for Lora multi-SF channel 1\n");
-		++err;
+	/* set configuration for Lora standard channel */
+	memset(&ifconf, 0, sizeof(ifconf)); /* initialize configuration structure */
+	if (json_value_get_type(json_object_get_value(conf, "chan_Lora_std")) != JSONObject) { /* fetch value (if possible) */
+		MSG("INFO: no configuration for Lora standard channel\n");
 	}
-	
-	ifconf.enable = true;
-	ifconf.rf_chain = 0;
-	ifconf.freq_hz = 62500;
-	ifconf.datarate = DR_LORA_MULTI;
-	i = lgw_rxif_setconf(2, ifconf); /* chain 2: Lora 125kHz, all SF, on 867.7 MHz */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for Lora multi-SF channel 2\n");
-		++err;
+	val = json_object_dotget_value(conf, "chan_Lora_std.enable");
+	if (json_value_get_type(val) == JSONBoolean) {
+		ifconf.enable = (bool)json_value_get_boolean(val);
 	}
-	
-	ifconf.enable = true;
-	ifconf.rf_chain = 1;
-	ifconf.freq_hz = -62500;
-	ifconf.datarate = DR_LORA_MULTI;
-	i = lgw_rxif_setconf(3, ifconf); /* chain 3: Lora 125kHz, all SF, on 868.3 MHz */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for Lora multi-SF channel 3\n");
-		++err;
+	if (ifconf.enable == false) {
+		MSG("INFO: Lora standard channel %i disabled\n", i);
+	} else  {
+		ifconf.rf_chain = (uint32_t)json_object_dotget_number(conf, "chan_Lora_std.radio");
+		ifconf.freq_hz = (int32_t)json_object_dotget_number(conf, "chan_Lora_std.if");
+		bw = (uint32_t)json_object_dotget_number(conf, "chan_Lora_std.bandwidth");
+		switch(bw) {
+			case 500000: ifconf.bandwidth = BW_500KHZ; break;
+			case 250000: ifconf.bandwidth = BW_250KHZ; break;
+			case 125000: ifconf.bandwidth = BW_125KHZ; break;
+			default: ifconf.bandwidth = BW_UNDEFINED;
+		}
+		sf = (uint32_t)json_object_dotget_number(conf, "chan_Lora_std.spread_factor");
+		switch(sf) {
+			case  7: ifconf.datarate = DR_LORA_SF7;  break;
+			case  8: ifconf.datarate = DR_LORA_SF8;  break;
+			case  9: ifconf.datarate = DR_LORA_SF9;  break;
+			case 10: ifconf.datarate = DR_LORA_SF10; break;
+			case 11: ifconf.datarate = DR_LORA_SF11; break;
+			case 12: ifconf.datarate = DR_LORA_SF12; break;
+			default: ifconf.datarate = DR_UNDEFINED;
+		}
+		MSG("INFO: Lora standard channel enabled, radio %i selected, IF %i Hz, %u Hz bandwidth, SF %u\n", ifconf.rf_chain, ifconf.freq_hz, bw, sf);
 	}
-	
-	/* set configuration for Lora 'stand alone' channel */
-	ifconf.enable = true;
-	ifconf.rf_chain = 1;
-	ifconf.freq_hz = 125000;
-	ifconf.bandwidth = BW_250KHZ;
-	ifconf.datarate = DR_LORA_SF10;
-	i = lgw_rxif_setconf(8, ifconf); /* chain 8: Lora 250kHz, SF10, on 866.0 MHz */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for Lora stand-alone channel\n");
-		++err;
+	if (lgw_rxif_setconf(8, ifconf) != LGW_HAL_SUCCESS) {
+		MSG("WARNING: invalid configuration for Lora standard channel\n", i);
 	}
 	
 	/* set configuration for FSK channel */
-	ifconf.enable = true;
-	ifconf.rf_chain = 1;
-	ifconf.freq_hz = 125000;
-	ifconf.bandwidth = BW_250KHZ;
-	ifconf.datarate = 64000;
-	i = lgw_rxif_setconf(9, ifconf); /* chain 9: FSK 64kbps, fdev 32kHz, variable payload, on 868.0 MHz */
-	if (i != LGW_HAL_SUCCESS) {
-		MSG("WARNING: invalid configuration for FSK channel\n");
-		++err;
+	memset(&ifconf, 0, sizeof(ifconf)); /* initialize configuration structure */
+	if (json_value_get_type(json_object_get_value(conf, "chan_FSK")) != JSONObject) { /* fetch value (if possible) */
+		MSG("INFO: no configuration for FSK channel\n");
+	}
+	val = json_object_dotget_value(conf, "chan_FSK.enable");
+	if (json_value_get_type(val) == JSONBoolean) {
+		ifconf.enable = (bool)json_value_get_boolean(val);
+	}
+	if (ifconf.enable == false) {
+		MSG("INFO: FSK channel %i disabled\n", i);
+	} else  {
+		ifconf.rf_chain = (uint32_t)json_object_dotget_number(conf, "chan_FSK.radio");
+		ifconf.freq_hz = (int32_t)json_object_dotget_number(conf, "chan_FSK.if");
+		bw = (uint32_t)json_object_dotget_number(conf, "chan_FSK.bandwidth");
+		switch(bw) {
+			case 500000: ifconf.bandwidth = BW_500KHZ; break;
+			case 250000: ifconf.bandwidth = BW_250KHZ; break;
+			case 125000: ifconf.bandwidth = BW_125KHZ; break;
+			case  62500: ifconf.bandwidth = BW_62K5HZ; break;
+			case  31200: ifconf.bandwidth = BW_31K2HZ; break;
+			case  15600: ifconf.bandwidth = BW_15K6HZ; break;
+			case   7800: ifconf.bandwidth = BW_7K8HZ;  break;
+			default: ifconf.bandwidth = BW_UNDEFINED;
+		}
+		ifconf.datarate = (uint32_t)json_object_dotget_number(conf, "chan_FSK.datarate");
+		MSG("INFO: FSK channel enabled, radio %i selected, IF %i Hz, %u Hz bandwidth, %u bps datarate\n", ifconf.rf_chain, ifconf.freq_hz, bw, ifconf.datarate);
+	}
+	if (lgw_rxif_setconf(9, ifconf) != LGW_HAL_SUCCESS) {
+		MSG("WARNING: invalid configuration for FSK channel\n", i);
 	}
 	
-	MSG("INFO: %d configuration error(s)\n", err);
-	return;
+	json_value_free(root_val);
+	return 0;
+}
+
+int parse_gateway_configuration(const char * conf_file) {
+	const char conf_obj[] = "gateway_conf";
+	JSON_Value *root_val;
+	JSON_Object *root = NULL;
+	JSON_Object *conf = NULL;
+	JSON_Value *val;
+	const char * tmp_string;
+	unsigned long long ull = 0;
+	
+	/* try to parse JSON */
+	root_val = json_parse_file(conf_file);
+	root = json_value_get_object(root_val);
+	if (root == NULL) {
+		MSG("ERROR: %s id not a valid JSON file\n", conf_file);
+		exit(EXIT_FAILURE);
+	}
+	conf = json_object_get_object(root, conf_obj);
+	if (conf == NULL) {
+		MSG("INFO: %s does not contain a JSON object named %s\n", conf_file, conf_obj);
+		return -1;
+	} else {
+		MSG("INFO: %s does contain a JSON object named %s, parsing gateway parameters\n", conf_file, conf_obj);
+	}
+	
+	/* getting network parameters (only those necessary for the packet logger) */
+	sscanf(json_object_dotget_string(conf, "gateway_ID"), "%llx", &ull);
+	lgwm = ull;
+	MSG("INFO: gateway MAC address is configured to %016llX\n", ull);
+	
+	json_value_free(root_val);
+	return 0;
 }
 
 void open_log(void) {
@@ -229,19 +319,28 @@ int main(int argc, char **argv)
 {
 	int i, j, k; /* loop and temporary variables */
 	
+	/* configuration file related */
+	const char global_conf_fname[] = "global_conf.json"; /* contain global (typ. network-wide) configuration */
+	const char local_conf_fname[] = "local_conf.json"; /* contain node specific configuration, overwrite global parameters for parameters that are defined in both */
+	const char debug_conf_fname[] = "debug_conf.json"; /* if present, all other configuration files are ignored */
+	
+	/* allocate memory for packet fetching and processing */
+	struct lgw_pkt_rx_s rxpkt[16]; /* array containing up to 16 inbound packets metadata */
+	struct lgw_pkt_rx_s *p; /* pointer on a RX packet */
+	int nb_pkt;
+	
 	/* local timestamp variables until we get accurate GPS time */
 	struct timespec fetch_time;
 	struct tm * x;
 	char fetch_timestamp[30];
 	
 	/* parse command line options */
-	while ((i = getopt (argc, argv, "hr:c:")) != -1) {
+	while ((i = getopt (argc, argv, "hr:")) != -1) {
 		switch (i) {
 			case 'h':
 				MSG( "Available options:\n");
 				MSG( "-h print this help\n");
 				MSG( "-r <int> rotate log file every N seconds (-1 disable log rotation)\n");
-				MSG( "-c <filename> use specified configuration file instead of the default one\n");
 				exit(EXIT_FAILURE);
 			
 			case 'r':
@@ -250,10 +349,6 @@ int main(int argc, char **argv)
 					MSG( "ERROR: Invalid argument for -r option\n");
 					exit(EXIT_FAILURE);
 				}
-				break;
-			
-			case 'c':
-				strcpy(conf_file_name, optarg);
 				break;
 			
 			default:
@@ -269,19 +364,30 @@ int main(int argc, char **argv)
 	sigaction(SIGINT, &sigact, NULL);
 	sigaction(SIGTERM, &sigact, NULL);
 	
-	/* configuration data processing */
-	sprintf(lgwm_str, "%016llX", lgwm);
-	
-	/* try to open and parse configuration file */
-	conf_file = fopen(conf_file_name, "r");
-	if(conf_file == NULL) {
-		MSG("WARNING: impossible to open configuration file %s, applying default configuration\n", conf_file_name);
-		set_loragw_default_conf();
+	/* configuration files management */
+	if (access(debug_conf_fname, R_OK) == 0) {
+	/* if there is a debug conf, parse only the debug conf */
+		MSG("INFO: found debug configuration file %s, other configuration files will be ignored\n", debug_conf_fname);
+		parse_SX1301_configuration(debug_conf_fname);
+		parse_gateway_configuration(debug_conf_fname);
+	} else if (access(global_conf_fname, R_OK) == 0) {
+	/* if there is a global conf, parse it and then try to parse local conf  */
+		MSG("INFO: found global configuration file %s, trying to parse it\n", global_conf_fname);
+		parse_SX1301_configuration(global_conf_fname);
+		parse_gateway_configuration(global_conf_fname);
+		if (access(local_conf_fname, R_OK) == 0) {
+			MSG("INFO: found local configuration file %s, trying to parse it\n", local_conf_fname);
+			parse_SX1301_configuration(local_conf_fname);
+			parse_gateway_configuration(local_conf_fname);
+		}
+	} else if (access(local_conf_fname, R_OK) == 0) {
+	/* if there is only a local conf, parse it and that's all */
+		MSG("INFO: found local configuration file %s, trying to parse it\n", local_conf_fname);
+		parse_SX1301_configuration(local_conf_fname);
+		parse_gateway_configuration(local_conf_fname);
 	} else {
-		/* try to parse configuration file */
-		// TODO
-		MSG("WARNING: failed to parse configuration file %s, applying default configuration\n", conf_file_name);
-		set_loragw_default_conf();
+		MSG("ERROR: failed to find any configuration file named %s, %s or %s\n", global_conf_fname, local_conf_fname, debug_conf_fname);
+		exit(EXIT_FAILURE);
 	}
 	
 	/* starting the gateway */
@@ -293,10 +399,12 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	
+	/* transform the MAC address into a string */
+	sprintf(lgwm_str, "%016llX", lgwm);
+	
 	/* opening log file and writing CSV header*/
 	time(&now_time);
 	open_log();
-	
 	
 	/* main loop */
 	while ((quit_sig != 1) && (exit_sig != 1)) {
