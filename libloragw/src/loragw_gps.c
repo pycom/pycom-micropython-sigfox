pycom/pycom-micropython-sigfox

commit a85ab4026d83f1afbd48d09bada6a10bcc88be89

Fixed potential floating point problem in reference time slope calculation.

libloragw/src/loragw_gps.c
@@ -53,6 +53,7 @@ Maintainer: Sylvain Miermont
 	#define DEBUG_ARRAY(a,b,c)			for(a=0;a!=0;){}
 	#define CHECK_NULL(a)				if(a==NULL){return LGW_GPS_ERROR;}
 #endif
+#define TRACE() 		fprintf(stderr, "@ %s %d\n", __FUNCTION__, __LINE__);
 
 /* -------------------------------------------------------------------------- */
 /* --- PRIVATE CONSTANTS ---------------------------------------------------- */
@@ -470,20 +471,21 @@ int lgw_gps_sync(struct tref *ref, uint32_t count_us, struct timespec utc) {
 	CHECK_NULL(ref);
 	
 	/* calculate the slope */
-	cnt_diff = (count_us - ref->count_us) / TS_CPS; /* uncorrected by xtal_err */
-	utc_diff = (utc.tv_sec - (ref->utc).tv_sec) + 1E-9 * (utc.tv_nsec - (ref->utc).tv_nsec);
-	if (utc_diff == 0.0) { // prevent divide by zero
-		DEBUG_MSG("ERROR: ATTEMPT TO DIVIDE BY ZERO\n");
-		return LGW_GPS_ERROR;
-	}
+	cnt_diff = (double)(count_us - ref->count_us) / (double)(TS_CPS); /* uncorrected by xtal_err */
+	utc_diff = (double)(utc.tv_sec - (ref->utc).tv_sec) + (1E-9 * (double)(utc.tv_nsec - (ref->utc).tv_nsec));
 	
 	/* detect aberrant points by measuring if slope limits are exceeded */
-	slope = cnt_diff/utc_diff;
-	if ((slope > PLUS_10PPM) || (slope < MINUS_10PPM)) {
-		DEBUG_MSG("Warning: correction range exceeded\n");
-		aber_n0 = true;
+	if (utc_diff < .1) { // prevent divide by zero
+		slope = cnt_diff/utc_diff;
+		if ((slope > PLUS_10PPM) || (slope < MINUS_10PPM)) {
+			DEBUG_MSG("Warning: correction range exceeded\n");
+			aber_n0 = true;
+		} else {
+			aber_n0 = false;
+		}
 	} else {
-		aber_n0 = false;
+		DEBUG_MSG("Warning: aberrant UTC value for synchronization\n");
+		aber_n0 = true;
 	}
 	
 	/* watch if the 3 latest sync point were aberrant or not */

commit ef02e0a3383fc786aa3b5b5ebbb17fed8dac8ca4

Fixed a bug introduced in a85ab4026d83f1afbd48d09bada6a10bcc88be89

libloragw/src/loragw_gps.c
@@ -478,7 +478,7 @@ int lgw_gps_sync(struct tref *ref, uint32_t count_us, struct timespec utc) {
 	utc_diff = (double)(utc.tv_sec - (ref->utc).tv_sec) + (1E-9 * (double)(utc.tv_nsec - (ref->utc).tv_nsec));
 	
 	/* detect aberrant points by measuring if slope limits are exceeded */
-	if (utc_diff < .1) { // prevent divide by zero
+	if (utc_diff != 0) { // prevent divide by zero
 		slope = cnt_diff/utc_diff;
 		if ((slope > PLUS_10PPM) || (slope < MINUS_10PPM)) {
 			DEBUG_MSG("Warning: correction range exceeded\n");