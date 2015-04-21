7e29a00017a58483654a15e6beea218823a7952a
Michael Coracin <mcoracin@semtech.com>
2015-04-21 14:35:01 +0200

    HAL: Force DAC gain to be set to 3 for now
    Until calibration supports different values

--- libloragw/src/loragw_hal.c
+++ libloragw/src/loragw_hal.c
@@ -770,15 +770,15 @@ int lgw_txgain_setconf(struct lgw_tx_gain_lut_s *conf) {
 			DEBUG_MSG("ERROR: TX gain LUT: SX1301 digital gain must be between 0 and 3\n");
 			return LGW_HAL_ERROR;
 		}
-		if (conf->lut[i].dac_gain > 3) {
-			DEBUG_MSG("ERROR: TX gain LUT: SX1257 DAC gain must not exceed 3\n");
+		if (conf->lut[i].dac_gain != 3) {
+			DEBUG_MSG("ERROR: TX gain LUT: SX1257 DAC gains != 3 are not supported\n");
 			return LGW_HAL_ERROR;
 		}
 		if (conf->lut[i].mix_gain > 15) {
 			DEBUG_MSG("ERROR: TX gain LUT: SX1257 mixer gain must not exceed 15\n");
 			return LGW_HAL_ERROR;
 		} else if (conf->lut[i].mix_gain < 8) {
-			DEBUG_MSG("ERROR: TX gain LUT: SX1257 mixer gains < 8 are not supported by calibration\n");
+			DEBUG_MSG("ERROR: TX gain LUT: SX1257 mixer gains < 8 are not supported\n");
 			return LGW_HAL_ERROR;
 		}
 		if (conf->lut[i].pa_gain > 3) {
