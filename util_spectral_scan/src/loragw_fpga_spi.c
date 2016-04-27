cc2d5bf07089b5809759afa2bfba223809013e7c
Michael Coracin <mcoracin@semtech.com>
2016-04-27 11:57:47 +0200

    util_spectral_scan: changed cs_change to 0

--- util_spectral_scan/src/loragw_fpga_spi.c
+++ util_spectral_scan/src/loragw_fpga_spi.c
@@ -182,7 +182,7 @@ int lgw_fpga_spi_w(void *spi_target, uint8_t spi_mux_dev, uint8_t address, uint8
     k.tx_buf = (unsigned long) out_buf;
     k.len = ARRAY_SIZE(out_buf);
     k.speed_hz = SPI_SPEED;
-    k.cs_change = 1;
+    k.cs_change = 0;
     k.bits_per_word = 8;
     a = ioctl(spi_device, SPI_IOC_MESSAGE(1), &k);
     
@@ -225,7 +225,7 @@ int lgw_fpga_spi_r(void *spi_target, uint8_t spi_mux_dev, uint8_t address, uint8
     k.tx_buf = (unsigned long) out_buf;
     k.rx_buf = (unsigned long) in_buf;
     k.len = ARRAY_SIZE(out_buf);
-    k.cs_change = 1;
+    k.cs_change = 0;
     a = ioctl(spi_device, SPI_IOC_MESSAGE(1), &k);
 
     /* determine return code */
@@ -273,7 +273,7 @@ int lgw_fpga_spi_wb(void *spi_target, uint8_t spi_mux_dev, uint8_t address, uint
     k[0].tx_buf = (unsigned long) &command[0];
     k[0].len = 2;
     k[0].cs_change = 0;
-    k[1].cs_change = 1;
+    k[1].cs_change = 0;
     for (i=0; size_to_do > 0; ++i) {
         chunk_size = (size_to_do < LGW_BURST_CHUNK) ? size_to_do : LGW_BURST_CHUNK;
         offset = i * LGW_BURST_CHUNK;
@@ -328,7 +328,7 @@ int lgw_fpga_spi_rb(void *spi_target, uint8_t spi_mux_dev, uint8_t address, uint
     k[0].tx_buf = (unsigned long) &command[0];
     k[0].len = 2;
     k[0].cs_change = 0;
-    k[1].cs_change = 1;
+    k[1].cs_change = 0;
     for (i=0; size_to_do > 0; ++i) {
         chunk_size = (size_to_do < LGW_BURST_CHUNK) ? size_to_do : LGW_BURST_CHUNK;
         offset = i * LGW_BURST_CHUNK;
