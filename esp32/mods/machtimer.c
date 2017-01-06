d94c5f75fc52532e092c40649349d3143ecaffc5
Abilio Marques <abilio@pycom.io>
2017-01-06 20:14:42 +0100

    esp32/mods/machtimer.c: Implement sleep_us().

--- esp32/mods/machtimer.c
+++ esp32/mods/machtimer.c
@@ -21,7 +21,7 @@
 
 static uint64_t us_timer_calibration;
 
-STATIC IRAM_ATTR mp_obj_t delay_us(mp_obj_t delay);
+STATIC IRAM_ATTR mp_obj_t sleep_us(mp_obj_t delay);
 
 void calibrate_us_timer(void) {
     uint64_t t1, t2;
@@ -29,10 +29,10 @@ void calibrate_us_timer(void) {
     mp_obj_t tz = mp_obj_new_int(0);
 
     us_timer_calibration = 0;
-    delay_us(tz);
+    sleep_us(tz);
     primsk = MICROPY_BEGIN_ATOMIC_SECTION();
     timer_get_counter_value(TIMER_GROUP_0, TIMER_0, &t1);
-    delay_us(tz);
+    sleep_us(tz);
     timer_get_counter_value(TIMER_GROUP_0, TIMER_0, &t2);
     MICROPY_END_ATOMIC_SECTION(primsk);
     us_timer_calibration = t2 - t1;
@@ -50,9 +50,14 @@ void modtimer_init0(void) {
     calibrate_us_timer();
 }
 
-STATIC IRAM_ATTR mp_obj_t delay_us(mp_obj_t delay) {
+STATIC IRAM_ATTR mp_obj_t sleep_us(mp_obj_t delay) {
     uint64_t d = mp_obj_get_int(delay);
     uint64_t t;
+    bool unlock_gil = false;
+
+    if (d >= 1000) {
+        unlock_gil = true;
+    }
 
     d *= CLK_FREQ / 1000000;
 
@@ -60,19 +65,29 @@ STATIC IRAM_ATTR mp_obj_t delay_us(mp_obj_t delay) {
     timer_get_counter_value(TIMER_GROUP_0, TIMER_0, &t);
     d += t;
 
-    do {
-        timer_get_counter_value(TIMER_GROUP_0, TIMER_0, &t);
-    } while(t < d);
+    if (unlock_gil) {
+        MP_THREAD_GIL_EXIT();
+        do {
+            timer_get_counter_value(TIMER_GROUP_0, TIMER_0, &t);
+        } while(t < d);
+        MP_THREAD_GIL_ENTER();
+
+    } else {
+        do {
+            timer_get_counter_value(TIMER_GROUP_0, TIMER_0, &t);
+        } while(t < d);
+    }
 
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(delay_us_obj, delay_us);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(sleep_us_fun_obj, sleep_us);
+STATIC MP_DEFINE_CONST_STATICMETHOD_OBJ(sleep_us_obj, &sleep_us_fun_obj);
 
 STATIC const mp_map_elem_t mach_timer_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR___name__),            MP_OBJ_NEW_QSTR(MP_QSTR_timer)},
     { MP_OBJ_NEW_QSTR(MP_QSTR_Alarm),               (mp_obj_t)&mach_timer_alarm_type },
     { MP_OBJ_NEW_QSTR(MP_QSTR_Chrono),              (mp_obj_t)&mach_timer_chrono_type },
-    // { MP_OBJ_NEW_QSTR(MP_QSTR_delay_us),            (mp_obj_t)&delay_us_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_sleep_us),            (mp_obj_t)&sleep_us_obj },
 };
 
 STATIC MP_DEFINE_CONST_DICT(mach_timer_globals, mach_timer_globals_table);
