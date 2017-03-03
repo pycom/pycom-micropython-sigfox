a65807b9408121392072fbde7604edd941b2f4da
Daniel Campora <daniel@pycom.io>
2017-03-03 17:04:59 +0100

    es32/tools/lora: Add ABP and OTAA Nano Gateway examples.

--- esp32/tools/lora/nano-gateway/main.py
+++ esp32/tools/lora/nano-gateway/main.py
@@ -1,11 +1,11 @@
 """ LoPy LoRaWAN Nano Gateway example usage """
 
 import config
-from nanogateway2 import NanoGateway
+from nanogateway import NanoGateway
 
 
 nanogw = NanoGateway(id=config.GATEWAY_ID, frequency=config.LORA_FREQUENCY,
-                     dr=config.LORA_DR, ssid=config.WIFI_SSID,
+                     datarate=config.LORA_DR, ssid=config.WIFI_SSID,
                      password=config.WIFI_PASS, server=config.SERVER,
                      port=config.PORT, ntp=config.NTP, ntp_period=config.NTP_PERIOD_S)
 
