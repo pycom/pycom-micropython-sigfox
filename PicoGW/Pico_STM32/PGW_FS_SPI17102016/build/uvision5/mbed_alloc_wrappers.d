25a634daca4f7f6c5ea0215f9a801ce7c46e4c4d
Michael Coracin <mcoracin@semtech.com>
2017-04-06 15:21:38 +0200

    deleted STM32 project

--- PicoGW/Pico_STM32/PGW_FS_SPI17102016/build/uvision5/mbed_alloc_wrappers.d
+++
@@ -1,8 +0,0 @@
-.\build\uvision5\mbed_alloc_wrappers.o: mbed-dev/common/mbed_alloc_wrappers.cpp
-.\build\uvision5\mbed_alloc_wrappers.o: mbed_config.h
-.\build\uvision5\mbed_alloc_wrappers.o: mbed-dev/api/mbed_mem_trace.h
-.\build\uvision5\mbed_alloc_wrappers.o: mbed-dev/api/mbed_stats.h
-.\build\uvision5\mbed_alloc_wrappers.o: mbed-dev/api/toolchain.h
-.\build\uvision5\mbed_alloc_wrappers.o: mbed-dev/api/SingletonPtr.h
-.\build\uvision5\mbed_alloc_wrappers.o: mbed-dev/api/mbed_assert.h
-.\build\uvision5\mbed_alloc_wrappers.o: mbed-dev/api/PlatformMutex.h
