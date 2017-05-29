05e98e68fcf815614bf04e7dfedc0bbb8237c5c0
Michael Coracin <mcoracin@semtech.com>
2017-05-29 15:27:29 +0200

    HAL: updated MCU FW VERSION

--- libloragw/inc/loragw_mcu.h
+++ libloragw/inc/loragw_mcu.h
@@ -30,7 +30,7 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
 #define LGW_MCU_SUCCESS 0
 #define LGW_MCU_ERROR   -1
 
-#define STM32FWVERSION  0x010a0002 /* incremement LSB for new version */
+#define STM32FWVERSION  0x010a0003 /* incremement LSB for new version */
 
 /* -------------------------------------------------------------------------- */
 /* --- PUBLIC TYPES --------------------------------------------------------- */
