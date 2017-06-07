4bc95bba79667a77510feb3c72f233142098b973
Daniel Campora <daniel@pycom.io>
2017-06-07 17:32:43 +0200

    esp32: Don't use gettimeofday() inside interrupts. Increase ISR stack size to 4K.

--- drivers/sx127x/sx1272/sx1272.c
+++ drivers/sx127x/sx1272/sx1272.c
@@ -1039,6 +1039,8 @@ static IRAM_ATTR void SX1272OnDioIrq (void) {
     }
 }
 
+extern uint64_t system_get_rtc_time(void);
+
 IRAM_ATTR void SX1272OnDio0Irq( void )
 {
     volatile uint8_t irqFlags = 0;
@@ -1054,11 +1056,9 @@ IRAM_ATTR void SX1272OnDio0Irq( void )
             case MODEM_LORA:
                 {
                     int8_t snr = 0;
-                    struct timeval tv;
 
                     // Store the packet timestamp
-                    gettimeofday(&tv, NULL);
-                    SX1272.Settings.LoRaPacketHandler.TimeStamp = (tv.tv_sec * 1000000) + tv.tv_usec;
+                    SX1272.Settings.LoRaPacketHandler.TimeStamp = system_get_rtc_time();
 
                     // Clear Irq
                     SX1272Write( REG_LR_IRQFLAGS, RFLR_IRQFLAGS_RXDONE );
