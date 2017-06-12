73a0ca6a43f4490e4ba928995befa5f513ce5b27
Daniel Campora <daniel@pycom.io>
2017-06-12 22:47:11 +0200

    esp32: Bump version to 1.7.2.b1.

--- esp32/pycom_version.h
+++ esp32/pycom_version.h
@@ -10,7 +10,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SW_VERSION_NUMBER                              "1.7.1.b1"
+#define SW_VERSION_NUMBER                              "1.7.2.b1"
 
 #define LORAWAN_VERSION_NUMBER                         "1.0.0"
 
