b5de6b751d5ff65a10417d69eb149a57fd228b9a
Robert HH <robert@hammelrath.com>
2017-08-27 10:32:35 +0200

    mpconfigport.h: Enable running pre-compiled scripts from the file system
    This feature is supported in the code, but not enabled yet. Enabling it
    allows to run code pre-compiled with mpy-cross without the need to embed it
    into the formware as frozen bytecode. Running bytecode from the file system
    saves the compiling phase, allowing larger scripts and giving faster load time.

--- esp32/mpconfigport.h
+++ esp32/mpconfigport.h
@@ -42,6 +42,7 @@
 #include <stdint.h>
 
 // options to control how Micro Python is built
+#define MICROPY_PERSISTENT_CODE_LOAD        (1)
 #define MICROPY_OBJ_REPR                            (MICROPY_OBJ_REPR_A)
 #define MICROPY_ALLOC_PATH_MAX                      (128)
 #define MICROPY_EMIT_X64                            (0)
