eefd6f4cbc76340062ed131b337f7a4c108e93b6
Daniel Campora <daniel@pycom.io>
2018-03-04 18:28:52 +0100

    esp32: Make complicance script configurable to support all 4 regions and both activation methods.

--- esp32/tools/lora/certification/main.py
+++ esp32/tools/lora/certification/main.py
@@ -1,5 +1,48 @@
 from network import LoRa
-# from certification import Compliance
+from certification import Compliance
 
-compliance = Compliance(region=LoRa.EU868, activation=LoRa.OTAA)
+regions = [LoRa.EU868, LoRa.AS923, LoRa.US915, LoRa.AU915]
+activations = [LoRa.OTAA, LoRa.ABP]
+
+regions_str = ['EU868', 'AS923', 'US915', 'AU915']
+activations_str = ['OTAA', 'ABP']
+
+region = None
+activation = None
+
+print('<<< Device ready for LoRaWAN certification testing >>>')
+print('')
+
+while True:
+    print('Please select the region:')
+    print('     (1) EU868')
+    print('     (2) AS923')
+    print('     (3) US915')
+    print('     (4) AU915')
+    region_in = int(input()) - 1
+
+    if region_in >= 0:
+        try:
+            region = regions[region_in]
+            break
+        except Exception:
+            pass
+
+while True:
+    print('Please select an activation method:')
+    print('     (1) OTAA')
+    print('     (2) ABP')
+    activation_in = int(input()) - 1
+
+    if activation_in >= 0:
+        try:
+            activation = regions[activation_in]
+            break
+        except Exception:
+            pass
+
+print('<<< Certification test started for region {} using {} >>>'.format(regions_str[region_in], activations_str[activation_in]))
+print('')
+
+compliance = Compliance(region=region, activation=activation)
 compliance.run()
