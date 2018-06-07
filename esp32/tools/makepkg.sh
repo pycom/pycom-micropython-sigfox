e9b5930d2f327d56110b1ba7721efcbb70aaa00f
Christian Ehlers <christian@ehlers.me.uk>
2018-06-07 02:30:08 +0200

    Add success message, bugfixes

--- esp32/tools/makepkg.sh
+++ esp32/tools/makepkg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 if [ -z $1 ]; then echo "Invalid board name!"; exit 1; fi
+if ! [ $0 = "tools/makepkg.sh" ]; then echo "Need to run as tools/makepkg.sh!"; exit 1; fi
 BOARD_NAME=$(echo $1 | tr '[IOY]' '[ioy]')
 BOARD_NAME_L=$(echo ${BOARD_NAME} | tr '[A-Z]' '[a-z]')
 VERSION=$(cat pycom_version.h |grep SW_VERSION_NUMBER | cut -d'"' -f2)
@@ -11,6 +12,7 @@ cp ../bootloader/bootloader.bin .
 cp ../lib/partitions.bin .
 cp ../${BOARD_NAME_L}.bin .
 cat ../../../../tools/script | sed s/"appimg.bin"/"${BOARD_NAME_L}.bin"/g > ./script
-tar -cvzf ../../../${FILE_NAME} bootloader.bin partitions.bin ${BOARD_NAME_L}.bin script
+tar -czf ../../../${FILE_NAME} bootloader.bin partitions.bin ${BOARD_NAME_L}.bin script
 cd ..
-rm -rf release_package
+echo "Release package build/${FILE_NAME} created successfully."
+rm -rf firmware_package
