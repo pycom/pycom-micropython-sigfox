526ef031f6befeed3f10189fec7514825d331ebb
Christian Ehlers <christian@ehlers.me.uk>
2018-06-16 01:04:42 +0200

    Simplify makepkg.sh script

--- esp32/tools/makepkg.sh
+++ esp32/tools/makepkg.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-REALPATH="realpath"
 #set -x
 set -e
 if [ -z $1 ]; then echo >&2 "Invalid board name!"; exit 1; fi
 if ! [ $0 = "tools/makepkg.sh" ]; then echo >&2 "Need to run as tools/makepkg.sh!"; exit 1; fi
 if ! [ -r "pycom_version.h" ]; then echo >&2 "Cannot read pycom_version.h! Aborting."; exit 1; fi
-${REALPATH} --help >/dev/null 2>&1 || { REALPATH="echo"; }
 if [ -z $2 ]; then
   RELEASE_DIR="$(pwd)/build"
 else
-  RELEASE_DIR=$($REALPATH $2 || echo $2)
+  RELEASE_DIR=$(realpath $2 || echo $2)
   if ! [ -d $RELEASE_DIR ]; then
     mkdir -p $RELEASE_DIR >/dev/null 2>&1 || { echo >&2 "Cannot create release directory! Aborting."; exit 1; }
   fi
@@ -18,7 +16,7 @@ else
     exit 1
   fi
 fi
-RELEASE_DIR=$($REALPATH $2 || echo $2)
+RELEASE_DIR=$(realpath $2 || echo $2)
 echo "Creating release package in ${RELEASE_DIR}"
 BOARD_NAME=$(echo $1 | tr '[IOY]' '[ioy]')
 BOARD_NAME_L=$(echo ${BOARD_NAME} | tr '[A-Z]' '[a-z]')
