34218d528a9f25af0b8737b7089087d27b70f806
iwahdan88 <islam@pycom.io>
2018-12-06 14:30:16 +0100

    Updated  script to default to local lib folders if IDF is not already built

--- esp32/get_idf_libs.py
+++ esp32/get_idf_libs.py
@@ -11,49 +11,55 @@ def main():
     cmd_args = cmd_parser.parse_args()
 
     src = cmd_args.idflibs
+    if not os.path.isdir(src):
+        print("IDF Libs Directory not Found defaulting to Local Lib Folders!")
+        sys.exit(0)
 
     # copy the bootloader libraries
     dst = os.getcwd() + '/bootloader/lib'
-
-    shutil.copy(src + '/bootloader/bootloader_support/libbootloader_support.a', dst)
-    shutil.copy(src + '/bootloader/log/liblog.a', dst)
-    shutil.copy(src + '/bootloader/micro-ecc/libmicro-ecc.a', dst)
-    shutil.copy(src + '/bootloader/soc/libsoc.a', dst)
-    shutil.copy(src + '/bootloader/spi_flash/libspi_flash.a', dst)
-
-
-    # copy the application libraries
-    dst = os.getcwd() + '/lib'
-
-    shutil.copy(src + '/bootloader_support/libbootloader_support.a', dst)
-    shutil.copy(src + '/bt/libbt.a', dst)
-    shutil.copy(src + '/cxx/libcxx.a', dst)
-    shutil.copy(src + '/driver/libdriver.a', dst)
-    shutil.copy(src + '/esp_adc_cal/libesp_adc_cal.a', dst)
-    shutil.copy(src + '/esp32/libesp32.a', dst)
-    shutil.copy(src + '/smartconfig_ack/libsmartconfig_ack.a', dst)
-    shutil.copy(src + '/expat/libexpat.a', dst)
-    shutil.copy(src + '/freertos/libfreertos.a', dst)
-    shutil.copy(src + '/heap/libheap.a', dst)
-    shutil.copy(src + '/jsmn/libjsmn.a', dst)
-    shutil.copy(src + '/json/libjson.a', dst)
-    shutil.copy(src + '/log/liblog.a', dst)
-    shutil.copy(src + '/lwip/liblwip.a', dst)
-    shutil.copy(src + '/mbedtls/libmbedtls.a', dst)
-    shutil.copy(src + '/micro-ecc/libmicro-ecc.a', dst)
-    shutil.copy(src + '/newlib/libnewlib.a', dst)
-    shutil.copy(src + '/nghttp/libnghttp.a', dst)
-    shutil.copy(src + '/nvs_flash/libnvs_flash.a', dst)
-    shutil.copy(src + '/openssl/libopenssl.a', dst)
-    shutil.copy(src + '/pthread/libpthread.a', dst)
-    shutil.copy(src + '/sdmmc/libsdmmc.a', dst)
-    shutil.copy(src + '/soc/libsoc.a', dst)
-    shutil.copy(src + '/spi_flash/libspi_flash.a', dst)
-    shutil.copy(src + '/tcpip_adapter/libtcpip_adapter.a', dst)
-    shutil.copy(src + '/vfs/libvfs.a', dst)
-    shutil.copy(src + '/wpa_supplicant/libwpa_supplicant.a', dst)
-    shutil.copy(src + '/xtensa-debug-module/libxtensa-debug-module.a', dst)
-    shutil.copy(src + '/openthread/libopenthread.a', dst)
+    try:
+        shutil.copy(src + '/bootloader/bootloader_support/libbootloader_support.a', dst)
+        shutil.copy(src + '/bootloader/log/liblog.a', dst)
+        shutil.copy(src + '/bootloader/micro-ecc/libmicro-ecc.a', dst)
+        shutil.copy(src + '/bootloader/soc/libsoc.a', dst)
+        shutil.copy(src + '/bootloader/spi_flash/libspi_flash.a', dst)
+    
+    
+        # copy the application libraries
+        dst = os.getcwd() + '/lib'
+    
+        shutil.copy(src + '/bootloader_support/libbootloader_support.a', dst)
+        shutil.copy(src + '/bt/libbt.a', dst)
+        shutil.copy(src + '/cxx/libcxx.a', dst)
+        shutil.copy(src + '/driver/libdriver.a', dst)
+        shutil.copy(src + '/esp_adc_cal/libesp_adc_cal.a', dst)
+        shutil.copy(src + '/esp32/libesp32.a', dst)
+        shutil.copy(src + '/smartconfig_ack/libsmartconfig_ack.a', dst)
+        shutil.copy(src + '/expat/libexpat.a', dst)
+        shutil.copy(src + '/freertos/libfreertos.a', dst)
+        shutil.copy(src + '/heap/libheap.a', dst)
+        shutil.copy(src + '/jsmn/libjsmn.a', dst)
+        shutil.copy(src + '/json/libjson.a', dst)
+        shutil.copy(src + '/log/liblog.a', dst)
+        shutil.copy(src + '/lwip/liblwip.a', dst)
+        shutil.copy(src + '/mbedtls/libmbedtls.a', dst)
+        shutil.copy(src + '/micro-ecc/libmicro-ecc.a', dst)
+        shutil.copy(src + '/newlib/libnewlib.a', dst)
+        shutil.copy(src + '/nghttp/libnghttp.a', dst)
+        shutil.copy(src + '/nvs_flash/libnvs_flash.a', dst)
+        shutil.copy(src + '/openssl/libopenssl.a', dst)
+        shutil.copy(src + '/pthread/libpthread.a', dst)
+        shutil.copy(src + '/sdmmc/libsdmmc.a', dst)
+        shutil.copy(src + '/soc/libsoc.a', dst)
+        shutil.copy(src + '/spi_flash/libspi_flash.a', dst)
+        shutil.copy(src + '/tcpip_adapter/libtcpip_adapter.a', dst)
+        shutil.copy(src + '/vfs/libvfs.a', dst)
+        shutil.copy(src + '/wpa_supplicant/libwpa_supplicant.a', dst)
+        shutil.copy(src + '/xtensa-debug-module/libxtensa-debug-module.a', dst)
+        shutil.copy(src + '/openthread/libopenthread.a', dst)
+    except:
+        print("WARNING: Couldn't get IDF libs defaulting to Local Lib Folders!")
+        
 
 if __name__ == "__main__":
     main()
