b09f59b96acfa3ba4c0684bdc92a20d9363c7d5e
iwahdan88 <islam@pycom.io>
2019-08-20 18:14:34 +0200

    Updated Pybytes version to v1.0.0

--- esp32/frozen/Pybytes/_pybytes.py
+++ esp32/frozen/Pybytes/_pybytes.py
@@ -1,9 +1,19 @@
+'''
+Copyright (c) 2019, Pycom Limited.
+This software is licensed under the GNU GPL version 3 or any
+later version, with permitted additional terms. For more information
+see the Pycom Licence v1.0 document supplied with this file, or
+available at https://www.pycom.io/opensource/licensing
+'''
+
 import os
 from machine import Timer
+
 try:
     from pybytes_connection import PybytesConnection
 except:
     from _pybytes_connection import PybytesConnection
+
 try:
     from pybytes_debug import print_debug
 except:
