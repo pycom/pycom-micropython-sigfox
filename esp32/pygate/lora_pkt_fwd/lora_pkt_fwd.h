bd300b86e864a11afa43b441f5d191779e5c3e95
Oliver <oliver@pycom.io>
2019-09-23 04:52:12 +0200

    Added missing header file

---
+++ esp32/pygate/lora_pkt_fwd/lora_pkt_fwd.h
@@ -0,0 +1,23 @@
+/*
+ / _____)             _              | |
+( (____  _____ ____ _| |_ _____  ____| |__
+ \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ _____) ) ____| | | || |_| ____( (___| | | |
+(______/|_____)_|_|_| \__)_____)\____)_| |_|
+  (C)2013 Semtech-Cycleo
+
+Description:
+    LoRa concentrator : Packet Forwarder trace helpers
+
+License: Revised BSD License, see LICENSE.TXT file include in the project
+Maintainer: Michael Coracin
+*/
+
+
+#ifndef _LORA_PKTFWD_H
+#define _LORA_PKTFWD_H
+
+int lora_gw_init(char *);
+
+#endif
+/* --- EOF ------------------------------------------------------------------ */
