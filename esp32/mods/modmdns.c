c79bb20a0037a0f372c3f0e3c5e78a1d4f3bd23f
Geza Husi <Geza Husi>
2019-11-22 18:15:33 +0100

    Query works

--- esp32/mods/modmdns.c
+++ esp32/mods/modmdns.c
@@ -12,8 +12,10 @@
 #include "py/obj.h"
 #include "py/runtime.h"
 #include "py/gc.h"
+#include <string.h>
 
 #include "mdns.h"
+#include "netutils.h"
 
 #include "modmdns.h"
 #include "modnetwork.h"
@@ -28,7 +30,14 @@
 /******************************************************************************
  DEFINE PRIVATE TYPES
  ******************************************************************************/
-
+typedef struct mod_mdns_query_obj_s {
+    mp_obj_base_t base;
+    mp_obj_t instance_name;       /* instance name */
+    mp_obj_t hostname;            /* hostname */
+    mp_obj_t port;                /* service port */
+    mp_obj_t txt;                 /* txt record */
+    mp_obj_t addr;                /* linked list of IP addresses found */
+}mod_mdns_query_obj_t;
 /******************************************************************************
  DECLARE PRIVATE FUNCTIONS
  ******************************************************************************/
@@ -37,6 +46,7 @@
  DEFINE PRIVATE VARIABLES
  ******************************************************************************/
 STATIC bool initialized = false;
+STATIC const mp_obj_type_t mod_mdns_query_type;
 
 /******************************************************************************
  DEFINE PRIVATE FUNCTIONS
@@ -126,7 +136,7 @@ STATIC mp_obj_t mod_mdns_add_service(mp_uint_t n_args, const mp_obj_t *pos_args,
             { MP_QSTR_service_type,             MP_ARG_OBJ  | MP_ARG_REQUIRED, },
             { MP_QSTR_proto,                    MP_ARG_INT  | MP_ARG_REQUIRED, },
             { MP_QSTR_port,                     MP_ARG_INT  | MP_ARG_REQUIRED, },
-            { MP_QSTR_text,                     MP_ARG_OBJ  | MP_ARG_KW_ONLY,  {.u_obj = MP_OBJ_NULL}},
+            { MP_QSTR_text,                     MP_ARG_OBJ  | MP_ARG_KW_ONLY, {.u_obj = MP_OBJ_NULL}},
     };
 
     if(initialized == true) {
@@ -218,6 +228,141 @@ STATIC mp_obj_t mod_mdns_remove_service(mp_obj_t service_type_in, mp_obj_t proto
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_mdns_remove_service_obj, mod_mdns_remove_service);
 
 
+// Initiate a new query
+STATIC mp_obj_t mod_mdns_query(mp_uint_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+
+    STATIC const mp_arg_t mod_mdns_query_args[] = {
+            { MP_QSTR_timeout,                  MP_ARG_INT  | MP_ARG_REQUIRED, },
+            { MP_QSTR_service_type,             MP_ARG_OBJ  | MP_ARG_REQUIRED, },
+            { MP_QSTR_proto,                    MP_ARG_INT  | MP_ARG_REQUIRED, },
+            { MP_QSTR_instance_name,            MP_ARG_OBJ  | MP_ARG_KW_ONLY,  {.u_obj = MP_OBJ_NULL}},
+    };
+
+    if(initialized == true) {
+
+        mp_arg_val_t args[MP_ARRAY_SIZE(mod_mdns_query_args)];
+        mp_arg_parse_all(n_args, pos_args, kw_args, MP_ARRAY_SIZE(args), mod_mdns_query_args, args);
+
+        // Get timeout value
+        uint32_t timeout = args[0].u_int;
+
+        // Get service type
+        const char * service_type = mp_obj_str_get_str(args[1].u_obj);
+
+        // Get proto
+        const char * proto = NULL;
+        mp_int_t proto_num = args[2].u_int;
+        if(proto_num != MOD_MDNS_PROTO_TCP && proto_num != MOD_MDNS_PROTO_UDP) {
+            nlr_raise(mp_obj_new_exception_msg(&mp_type_ValueError, "proto must be 0 (TCP) or 1 (UDP)"));
+        }
+        proto = proto_num == MOD_MDNS_PROTO_TCP ? "_tcp" : "_udp";
+
+        // Get instance name
+        const char * instance_name = NULL;
+        if(args[3].u_obj != NULL) {
+            instance_name = mp_obj_str_get_str(args[3].u_obj);
+        }
+
+
+        mdns_result_t *results;
+        esp_err_t ret;
+
+        if(instance_name == NULL) {
+            ret = mdns_query_ptr(service_type, proto, timeout, 10, &results);
+        }
+        else {
+            ret = mdns_query_srv(instance_name, service_type, proto, timeout, &results);
+        }
+
+
+        if(ret != ESP_OK) {
+            nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_RuntimeError, "Query failed, error code: %d", ret));
+        }
+        else {
+            mdns_result_t *result = results;
+            mp_obj_t queries_list = mp_obj_new_list(0, NULL);
+            while(result != NULL) {
+                mod_mdns_query_obj_t *query_obj = m_new(mod_mdns_query_obj_t, 1);
+                query_obj->base.type = (mp_obj_t)&mod_mdns_query_type;
+                query_obj->instance_name = mp_obj_new_str(result->instance_name, strlen(result->instance_name));
+                query_obj->hostname = mp_obj_new_str(result->hostname, strlen(result->hostname));
+                query_obj->port = mp_obj_new_int(result->port);
+
+                query_obj->txt = mp_obj_new_list(0, NULL);
+                for(int i = 0; i < result->txt_count; i++) {
+                    mp_obj_t tuple[2];
+                    tuple[0] = mp_obj_new_str(result->txt[i].key, strlen(result->txt[i].key));
+                    tuple[1] = mp_obj_new_str(result->txt[i].value, strlen(result->txt[i].value));
+                    mp_obj_list_append(query_obj->txt, mp_obj_new_tuple(2, tuple));
+                }
+                query_obj->addr = netutils_format_ipv4_addr((uint8_t *)&result->addr->addr.u_addr.ip4.addr, NETUTILS_BIG);
+
+                mp_obj_list_append(queries_list, query_obj);
+
+                result = result->next;
+            }
+
+            return queries_list;
+
+        }
+    }
+    else {
+        nlr_raise(mp_obj_new_exception_msg(&mp_type_RuntimeError, "MDNS module is not initialized!"));
+    }
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(mod_mdns_query_obj, 3, mod_mdns_query);
+
+STATIC mp_obj_t mod_mdns_query_instance_name(mp_obj_t self) {
+
+    return ((mod_mdns_query_obj_t *)self)->instance_name;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_mdns_query_instance_name_obj, mod_mdns_query_instance_name);
+
+STATIC mp_obj_t mod_mdns_query_host_name(mp_obj_t self) {
+
+    return ((mod_mdns_query_obj_t *)self)->hostname;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_mdns_query_host_name_obj, mod_mdns_query_host_name);
+
+STATIC mp_obj_t mod_mdns_query_port(mp_obj_t self) {
+
+    return ((mod_mdns_query_obj_t *)self)->port;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_mdns_query_port_obj, mod_mdns_query_port);
+
+STATIC mp_obj_t mod_mdns_query_txt(mp_obj_t self) {
+
+    return ((mod_mdns_query_obj_t *)self)->txt;
+
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_mdns_query_txt_obj, mod_mdns_query_txt);
+
+STATIC mp_obj_t mod_mdns_query_addr(mp_obj_t self) {
+
+    return ((mod_mdns_query_obj_t *)self)->addr;
+
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_mdns_query_addr_obj, mod_mdns_query_addr);
+
+STATIC const mp_map_elem_t mod_mdns_query_locals_dict_table[] = {
+    // instance methods
+    { MP_OBJ_NEW_QSTR(MP_QSTR_instance_name),           (mp_obj_t)&mod_mdns_query_instance_name_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_host_name),               (mp_obj_t)&mod_mdns_query_host_name_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_port),                    (mp_obj_t)&mod_mdns_query_port_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_txt),                     (mp_obj_t)&mod_mdns_query_txt_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_addr),                    (mp_obj_t)&mod_mdns_query_addr_obj },
+};
+STATIC MP_DEFINE_CONST_DICT(mod_mdns_query_locals_dict, mod_mdns_query_locals_dict_table);
+
+static const mp_obj_type_t mod_mdns_query_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_MDNS_Query,
+    .locals_dict = (mp_obj_t)&mod_mdns_query_locals_dict,
+};
+
+
 STATIC const mp_map_elem_t mod_mdns_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR___name__),                        MP_OBJ_NEW_QSTR(MP_QSTR_mdns) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_init),                            (mp_obj_t)&mod_mdns_init_obj },
@@ -225,6 +370,7 @@ STATIC const mp_map_elem_t mod_mdns_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_set_name),                        (mp_obj_t)&mod_mdns_set_name_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_add_service),                     (mp_obj_t)&mod_mdns_add_service_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_remove_service),                  (mp_obj_t)&mod_mdns_remove_service_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_query),                           (mp_obj_t)&mod_mdns_query_obj },
 
     // class constants
     { MP_OBJ_NEW_QSTR(MP_QSTR_PROTO_TCP),                     MP_OBJ_NEW_SMALL_INT(MOD_MDNS_PROTO_TCP) },
