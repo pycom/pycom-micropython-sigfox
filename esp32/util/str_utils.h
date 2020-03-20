454c76a7a371d645e277907813934e1e30d2c1b2
Peter Putz <peter@pycom.io>
2020-03-20 10:51:46 +0100

    add strlen_const() and sprint_binary_u8()

---
+++ esp32/util/str_utils.h
@@ -0,0 +1,17 @@
+#ifndef STR_UTILS_H
+#define STR_UTILS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * Determine the length of str at compile time
+ *
+ * The length is excluding the terminating \0 just as strlen()
+ * make sure you pass a compile time constant
+ */
+#define strlen_const(string) (sizeof(string)-1)
+
+void sprint_binary_u8(char* s, uint8_t v);
+
+#endif
