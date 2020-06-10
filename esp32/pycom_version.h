63d7be958ebeffdb9f5d4e4a959a0d9925b94df6
peter-pycom <61411318+peter-pycom@users.noreply.github.com>
2020-06-10 18:25:08 +0200

    v1.20.2.rc9 (#135)

--- esp32/pycom_version.h
+++ esp32/pycom_version.h
@@ -10,7 +10,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SW_VERSION_NUMBER                              "1.20.2.rc7"
+#define SW_VERSION_NUMBER                              "1.20.2.rc9"
 
 #define LORAWAN_VERSION_NUMBER                         "1.0.2"
 
