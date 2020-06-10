9f6e28e6afeec61ee36c93810afc4fabbe2f9dfc
Christian Ehlers <cehlers@pycom.io>
2020-06-10 16:00:23 +0200

    Update pybytes to 1.5.0 (#133)

--- esp32/pycom_version.h
+++ esp32/pycom_version.h
@@ -17,7 +17,7 @@
 #define SIGFOX_VERSION_NUMBER                          "1.0.1"
 
 #if (VARIANT == PYBYTES)
-#define PYBYTES_VERSION_NUMBER                         "1.4.0"
+#define PYBYTES_VERSION_NUMBER                         "1.5.0"
 #endif
 
 #endif /* VERSION_H_ */
