73f002139360291047b747c41a581c448240a3c9
mate-pycom <mate@pycom.io>
2020-06-10 12:30:27 +0200

    HTTP/S Server/Client Module (#118)

---
+++ esp32/mods/modhttp.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020, Pycom Limited.
+ *
+ * This software is licensed under the GNU GPL version 3 or any
+ * later version, with permitted additional terms. For more information
+ * see the Pycom Licence v1.0 document supplied with this file, or
+ * available at https://www.pycom.io/opensource/licensing
+ */
+
+#ifndef MODHTTP_H_
+#define MODHTTP_H_
+
+extern const mp_obj_module_t mod_http_server;
+extern const mp_obj_module_t mod_http_client;
+
+#endif // MODHTTP_H_
