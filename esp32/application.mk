3c72da7e9f469e67e2de340134af25a368f3edcc
Christian Ehlers <cehlers@pycom.io>
2020-06-25 20:51:00 +0200

    Add support for EU433 region
    Thanks to @robert-hh for the contribution via https://github.com/robert-hh/pycom-micropython-sigfox/commit/3b12fc5a019ec94a3728d7aeb24827e81d3d65fd
    
    Co-Authored-By: robert-hh <robert-hh@users.noreply.github.com>

--- esp32/application.mk
+++ esp32/application.mk
@@ -233,6 +233,7 @@ APP_LIB_LORA_SRC_C = $(addprefix lib/lora/,\
 	mac/region/RegionEU868.c \
 	mac/region/RegionUS915.c \
 	mac/region/RegionCN470.c \
+	mac/region/RegionEU433.c \
 	mac/region/RegionIN865.c \
 	system/delay.c \
 	system/gpio.c \
@@ -375,7 +376,7 @@ APP_LDFLAGS += $(LDFLAGS) -T esp32_out.ld -T esp32.project.ld -T esp32.rom.ld -T
 # add the application specific CFLAGS
 CFLAGS += $(APP_INC) -DMICROPY_NLR_SETJMP=1 -DMBEDTLS_CONFIG_FILE='"mbedtls/esp_config.h"' -DHAVE_CONFIG_H -DESP_PLATFORM -DFFCONF_H=\"lib/oofatfs/ffconf.h\" -DWITH_POSIX
 CFLAGS_SIGFOX += $(APP_INC) -DMICROPY_NLR_SETJMP=1 -DMBEDTLS_CONFIG_FILE='"mbedtls/esp_config.h"' -DHAVE_CONFIG_H -DESP_PLATFORM
-CFLAGS += -DREGION_AS923 -DREGION_AU915 -DREGION_EU868 -DREGION_US915 -DREGION_CN470 -DREGION_IN865 -DBASE=0 -DPYBYTES=1 
+CFLAGS += -DREGION_AS923 -DREGION_AU915 -DREGION_EU868 -DREGION_US915 -DREGION_CN470 -DREGION_EU433 -DREGION_IN865 -DBASE=0 -DPYBYTES=1 
 # Specify if this is base or Pybytes Firmware
 ifeq ($(VARIANT),BASE)
 CFLAGS += -DVARIANT=0
