64f2a7ab63028ae79f90d0a11da5b5fec75bf7c2
Geza Husi <geza@pycom.io>
2020-07-28 18:23:38 +0200

    Update sigfox libraries to be compliant with esp-idf 4.0 and recompile them with 2020r2 toolchain

--- esp32/get_sigfox_libs.py
+++ esp32/get_sigfox_libs.py
@@ -11,7 +11,6 @@ def main():
     try:
         shutil.copy('./build/FIPY/release/sigfox/sigfox.a',  './sigfox/modsigfox_FIPY.a')
         shutil.copy('./build/LOPY4/release/sigfox/sigfox.a', './sigfox/modsigfox_LOPY4.a')
-        shutil.copy('./build/SIPY/release/sigfox/sigfox.a',  './sigfox/modsigfox_SIPY.a')
     except:
         print("Couldn't copy Sigfox libs!")
         traceback.print_exc()
