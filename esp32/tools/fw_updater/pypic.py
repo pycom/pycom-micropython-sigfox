70ad79c9528eb7b1b10a2df6f44109399c4eddea
Christian Ehlers <cehlers@pycom.io>
2020-08-04 21:32:16 +0200

    Updater update (as from the public repo)
    Add support for python3
    Add support for sigfox config
    Add support for fs_type (pycom)
    Bugfixes, Formatting etc.

--- esp32/tools/fw_updater/pypic.py
+++ esp32/tools/fw_updater/pypic.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+
+#   Copyright (c) 2016-2020, Pycom Limited.
+#
+#   This software is licensed under the GNU GPL version 3 or any
+#   later version, with permitted additional terms. For more information
+#   see the Pycom Licence v1.0 document supplied with this file, or
+#   available at https://www.pycom.io/opensource/licensing
 
 from __future__ import print_function
 
@@ -13,7 +20,7 @@ try:
     from local_settings import DEBUG
 except:
     DEBUG = False
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 
 CMD_PEEK = (0x0)
 CMD_POKE = (0x01)
@@ -45,6 +52,7 @@ _ADCON0_GO_nDONE_MASK = (0x02)
 ADRESL_ADDR = (0x09B)
 ADRESH_ADDR = (0x09C)
 
+TRISA_ADDR = (0x08C)
 TRISC_ADDR = (0x08E)
 
 PORTA_ADDR = (0x00C)
@@ -77,7 +85,7 @@ def exit_with_error(code, msg):
 def warn(msg):
     eprint('warning:', msg)
 
-    
+
 def print_debug(msg):
     if DEBUG:
         eprint(msg)
@@ -89,12 +97,11 @@ class Pypic:
         # we need bytesize to be 5 bits in order for the PIC to process the commands
         try:
             self.serial = serial.Serial(port, baudrate=115200, bytesize=serial.FIVEBITS, timeout=0.25, exclusive=True)
-            print_debug("Connected in exclusive mode")
             connected = True
         except Exception as e:
             connected = False
-        if not connected:
             print_debug("Not connecting in exclusive mode because: %s" % str(e))
+        if not connected:
             try:
                 self.serial = serial.Serial(port, baudrate=115200, bytesize=serial.FIVEBITS, timeout=0.25)
             except Exception as e:
@@ -109,31 +116,43 @@ class Pypic:
         except Exception:
             pass
 
-    def _write(self, data, read=True):
+    def _write(self, data, read=True, num_read_bytes=1):
         self.serial.write(data)
         if read:
             r_data = self.serial.read(2)
             if not r_data:
                 raise Exception('Timeout while waiting for Rx data')
-            return struct.unpack('B', r_data[0])[0]
+            if num_read_bytes == 2:
+                (b1, b2) = struct.unpack('BB', r_data)
+                return 256 * b2 + b1
+            else:
+                if sys.version_info[0] < 3:
+                    return struct.unpack('B', r_data[0])[0]
+                else:
+                    return r_data[0]
 
-    def _send_cmd(self, cmd):
-        return self._write(bytearray([cmd]))
+    def _flush(self, num_read_bytes=10):
+        r_data = self.serial.read(num_read_bytes)
+        # print(r_data)
+        return
+
+    def _send_cmd(self, cmd, num_read_bytes=1):
+        return self._write(bytearray([cmd]), True, num_read_bytes)
 
     def read_hw_version(self):
-        return self._send_cmd(CMD_HW_VER)
+        return self._send_cmd(CMD_HW_VER, 2)
 
     def read_fw_version(self):
-        return self._send_cmd(CMD_FW_VER)
+        return self._send_cmd(CMD_FW_VER, 2)
 
     def read_product_id(self):
-        return self._send_cmd(CMD_PROD_ID)
+        return self._send_cmd(CMD_PROD_ID, 2)
 
     def peek_memory(self, addr):
         return self._write(bytearray([CMD_PEEK, addr & 0xFF, (addr >> 8) & 0xFF]))
 
     def poke_memory(self, addr, value):
-         self._write(bytearray([CMD_POKE, addr & 0xFF, (addr >> 8) & 0xFF, value & 0xFF]), False)
+        self._write(bytearray([CMD_POKE, addr & 0xFF, (addr >> 8) & 0xFF, value & 0xFF]), False)
 
     def magic_write_read(self, addr, _and=0xFF, _or=0, _xor=0):
         return self._write(bytearray([CMD_MAGIC, addr & 0xFF, (addr >> 8) & 0xFF, _and & 0xFF, _or & 0xFF, _xor & 0xFF]))
@@ -163,6 +182,11 @@ class Pypic:
         self.set_bits_in_memory(TRISC_ADDR, 1 << 5)
 
     def enter_pycom_programming_mode(self, reset=True):
+        print_debug("Entering programming mode with reset={}".format(reset))
+        # make RA5 an output
+        self.mask_bits_in_memory(TRISA_ADDR, ~(1 << 5))
+        # drive RA5 low
+        self.mask_bits_in_memory(PORTA_ADDR, ~(1 << 5))
         # make RC0 an output
         self.mask_bits_in_memory(TRISC_ADDR, ~(1 << 0))
         # set RC0 low
@@ -173,13 +197,20 @@ class Pypic:
         # We should keep RC0 low at this point in case someone
         # presses the reset button before the firmware upgrade
         # as this is mandatory for the regular expansion board
+        self._flush()
 
     def exit_pycom_programming_mode(self, reset=True):
+        print_debug("Leaving programming mode with reset={}".format(reset))
+        # make RA5 an output
+        self.mask_bits_in_memory(TRISA_ADDR, ~(1 << 5))
+        # drive RA5 high
+        self.set_bits_in_memory(PORTA_ADDR, 1 << 5)
         # make RC0 an input
         # This will prevent issues with the RGB LED
         self.set_bits_in_memory(TRISC_ADDR, 1 << 0)
         if reset:
             self.reset_pycom_module()
+        self._flush()
 
     def isdetected(self):
         return self.detected
@@ -209,6 +240,11 @@ def main(args):
             pic.enter_pycom_programming_mode(not args.noreset)
         elif args.exit:
             pic.exit_pycom_programming_mode(not args.noreset)
+
+        # print debug info about current PIC product
+        print_debug("read_product_id(): 0x%X" % pic.read_product_id())
+        print_debug("read_hw_version(): 0x%X" % pic.read_hw_version())
+        print_debug("read_fw_version(): 0x%X" % pic.read_fw_version())
 
     pic.close()
 
